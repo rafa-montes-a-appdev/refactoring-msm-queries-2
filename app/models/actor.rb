ba2e754de5eae01313b84a907fa0d6fef62ef3c5
Rafael Montes <rmontes@chicagobooth.edu>
2023-05-02 15:41:01 +0000

    Refactored MSM

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,26 +9,31 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
+
 class Actor < ApplicationRecord
-  def characters
-    key = self.id
+  
+  has_many(:characters, { :foreign_key => "actor_id", :class_name => "Character" })
+  
+  
+ # def characters
+   # key = self.id
+    #the_many = Character.where({ :actor_id => key })
+    #return the_many
+  #end
 
-    the_many = Character.where({ :actor_id => key })
 
-    return the_many
-  end
+  has_many(:filmography, { :through => "characters", :source => "movie"})
 
-  def filmography
-    the_many = Array.new
+  #def filmography
+    #the_many = Array.new
 
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
+    #self.characters.each do |joining_record|
+      #destination_record = joining_record.movie
 
-      the_many.push(destination_record)
-    end
+      #the_many.push(destination_record)
+    #end
 
-    return the_many
-  end
+    #return the_many
+  #end
 
 end
